97cdc710bbfa1133d8c43d3cdb9823eb98f3322e
emacip <emacip@eaglealpha.com>
2014-09-16 17:36:53 +0200

    first commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :facebook, '332123006830280', 'c93441215dbdd2e65f1b77a7dfdbab9c'
 end
